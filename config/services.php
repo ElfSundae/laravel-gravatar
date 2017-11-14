056a92ab73611da2dc13fa293c4ff6735b28f155
Elf Sundae <elf.sundae@gmail.com>
2017-11-14 16:42:32 +0800

    Update configuration

--- config/services.php
+++ config/services.php
@@ -7,26 +7,40 @@ return [
     | Gravatar - Globally Recognized Avatars - https://gravatar.com
     |--------------------------------------------------------------------------
     |
-    | All configuration value can be an empty string or null to use the
+    | Every configuration value can be an empty string or null to use the
     | Gravatar default value.
     |
-    | url      The root URL of Gravatar service.
-    |            https://secure.gravatar.com/avatar         (Default)
-    |            https://gravatar.cat.net/avatar            (China Mirror)
-    |            https://v2ex.assets.uxengine.net/gravatar  (China Mirror)
-    | size     The default avatar size in pixel, default is "80".
-    | default  The default avatar image.
-    |            404, mm, identicon, monsterid, wavatar, retro, blank, "http://image/url"
-    | rating   The highest avatar rating, default is "g".
-    |            g, pg, r, x
+    | Possible Keys:
+    |
+    | url           Root URL of Gravatar service:
+    |                   https://secure.gravatar.com/avatar         (Default)
+    |                   https://gravatar.cat.net/avatar            (China Mirror)
+    |                   https://v2ex.assets.uxengine.net/gravatar  (China Mirror)
+    | extension     File-type extension: jpg, png, etc.
+    | size / s      Avatar size in pixel, default is 80.
+    | default / d   The default avatar image:
+    |                   404, mm, identicon, monsterid, wavatar, retro,
+    |                   robohash, blank, urlencode("http://image/url")
+    | rating / r    The highest avatar rating, default is "g": g, pg, r, x.
+    | forcedefault / f  If for some reason you wanted to force the default image
+    |                   to always load, set this value to "y".
     |
     */
 
     'gravatar' => [
-        'url' => '',
-        'size' => '',
-        'default' => '',
-        'rating' => '',
+
+        'default' => [
+            'size' => 120,
+        ],
+
+        'small' => [
+            's' => 40,
+        ],
+
+        'large' => [
+            's' => 640,
+        ],
+
     ],
 
 ];
